0b3c80bdb862cb10bd3d22341607bf940c4c49a8
ПК <@DESKTOP-FU9UH67>
2023-03-04 12:13:19 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZ_25
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void press3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void press7Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Press9Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "7";
+        }
+
+        private void Press7Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "9";
+        }
+
+        private void Press8Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "8";
+        }
+
+        private void PressPlusButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "+";
+        }
+
+        private void Press6Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "4";
+        }
+
+        private void Press5Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "5";
+        }
+
+        private void Press4Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "6";
+        }
+
+        private void PressMinusButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "-";
+        }
+
+        private void Press1Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "1";
+        }
+
+        private void Press2Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "2";
+        }
+
+        private void Press3Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "3";
+        }
+
+        private void PressMultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "*";
+        }
+
+        private void Press0Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "0";
+        }
+
+        private void PressEqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "=";
+        }
+
+        private void PressSharingButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBlock.Text = "/";
+        }
+    }
+}
